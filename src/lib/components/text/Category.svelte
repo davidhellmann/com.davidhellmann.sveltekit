eda89e74d1b5e77e200f590f5202663657628b01
David Hellmann <david@hellmann.io>
2024-11-20 23:08:42 +0100

    💄 style(tape/deco): Update Styling

--- src/lib/components/text/Category.svelte
+++ src/lib/components/text/Category.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   import { tv, type VariantProps } from "tailwind-variants";
   import IconSprite from "$components/ui/IconSprite.svelte";
+  import type { HeroiconsIcons } from "$lib/types/heroicons-icons";
 
   const tvCategory = tv({
     base: "flex items-center gap-2 font-mono text-sm",
     variants: {
       variant: {
         blog: "",
-        work: "bg-neutral-900/10 border border-neutral-100/20 text-current px-3 pt-1.5 pb-1 rounded-md self-start"
+        work: "bg-neutral-900/10 border border-neutral-100/20 text-current px-2.5 pt-1.5 pb-1 rounded-lg self-start"
       }
     },
     defaultVariants: {
@@ -19,14 +20,32 @@
     compName?: string;
     className?: string;
     title: string;
+    icon?: HeroiconsIcons;
   } & VariantProps<typeof tvCategory>;
 
-  const { compName = "Category", className, title, variant }: CategoryProps = $props();
+  let { compName = "Category", className, title, variant, icon = "folder-open-outline" }: CategoryProps = $props();
+
+  switch (title) {
+  case "Client":
+    icon = "home-modern-outline";
+    break;
+  case "Agency":
+    icon = "building-office-outline";
+    break;
+  case "Side Project":
+    icon = "users-outline";
+    break;
+  default:
+    icon = "folder-open-outline";
+    break;
+  }
+
+  console.log(icon);
 </script>
 
 {#if title}
   <div data-comp={compName} class={tvCategory({ variant, className })}>
-    <IconSprite className="-translate-y-px" icon="folder-open-outline" size={"relative"} />
+    <IconSprite className="-translate-y-px" {icon} size={"relative"} />
     {title}
   </div>
 {/if}
