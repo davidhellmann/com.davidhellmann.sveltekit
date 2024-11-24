3ef7cc0da4c6a792270a8c028dcc77ca2d0750fb
David Hellmann <david@hellmann.io>
2024-11-24 01:22:58 +0100

    🎨 chore(seomatic): add sitemaps

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,7 +3,9 @@ import { type Handle } from "@sveltejs/kit";
 
 const preloadFonts: Handle = async ({ event, resolve }) => {
   return resolve(event, {
-    preload: ({ type }) => type === "font"
+    preload: ({ type }) => {
+      return type === "font" || type === "js" || type === "css";
+    }
   });
 };
 
