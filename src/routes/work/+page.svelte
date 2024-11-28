6c159abbad624d0f8ad5df1655ced5956735478e
David Hellmann <david@hellmann.io>
2024-11-28 09:57:46 +0100

    🎨 chore(code): mobile opti

--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -15,9 +15,6 @@
   let workEntry = getFirstEntry(data.workEntry);
   let entries = $derived(data.entries);
 
-  console.log(entries);
-
-
   const cc = {
     heading: "span-content text-olkch-pink is-zoomInDown text-7xl font-decorative font-extrabold flex flex-wrap",
     text: "span-content xl:col-start-[col-3] xl:col-end-[col-10] text-2xl is-zoomInDown [&_*_strong]:decoration-wavy [&_*_strong]:underline [&_*_strong]:decoration-4 [&_*_strong]:decoration-accent-purple-400",
@@ -36,11 +33,13 @@
     {/if}
   </div>
 
-  <ul class="span-content flex flex-col gap-1">
-    {#each entries as entry (entry.id)}
-      <li>
-        <a class="flex p-4 bg-neutral-900 rounded transition hover:bg-neutral-800" href={entry.url}>{entry.title}</a>
-      </li>
-    {/each}
-  </ul>
+  {#if entries}
+    <ul class="span-content flex flex-col gap-1">
+      {#each entries as entry (entry.id)}
+        <li>
+          <a class="flex p-4 bg-neutral-900 rounded transition hover:bg-neutral-800" href={entry.url}>{entry.title}</a>
+        </li>
+      {/each}
+    </ul>
+  {/if}
 {/if}
