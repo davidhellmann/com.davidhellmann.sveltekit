e60dd033aba595de649cee6dee83d3f89f7c6943
David Hellmann <david@hellmann.io>
2025-02-28 23:11:17 +0100

    🎨 chore: läuft

--- src/lib/components/sections/Footer.svelte
+++ src/lib/components/sections/Footer.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
   import { tv, type VariantProps } from "tailwind-variants";
   import { useWaypoint } from "$lib/actions/action.waypoint";
-
+  import { useFullWidthText } from "$lib/actions/action.fullWidthText";
 
   const tvFooter = tv({
     slots: {
-      slotBase: "fluid-grid w-full lg:max-w-[min(calc(100%-4vw),2000px)] left-1/2 -translate-x-1/2 mt-40 z-20 relative bg-[rgba(255,255,255,0.025)]  border-t border-neutral-500/20",
-      slotInner: "span-content py-16 is-zoomInUp"
+      slotBase:
+        "fluid-grid w-full lg:max-w-[min(calc(100%-4vw),2000px)] left-1/2 -translate-x-1/2 mt-40 z-20 relative bg-[rgba(255,255,255,0.025)]  border-t border-neutral-500/20",
+      slotInner: "span-full is-zoomInUp overflow-clip",
+      slotBigText: "translate-y-1/4 scale-105 uppercase font-decorative font-extrabold text-neutral-500/5 text-center"
     }
   });
 
@@ -15,17 +17,13 @@
     className?: string;
   } & VariantProps<typeof tvFooter>;
 
-  let {
-    compName = "Footer",
-    className,
-  }: FooterProps = $props();
+  let { compName = "Footer", className }: FooterProps = $props();
 
-  const { slotBase, slotInner } = tvFooter({ className });
-
+  const { slotBase, slotInner, slotBigText } = tvFooter({ className });
 </script>
 
-<footer class={`${slotBase({ className })}`}
-        data-comp={compName}
-        use:useWaypoint data-waypoint>
-  <div class={slotInner()} data-waypoint-target>Footer WIP</div>
+<footer class={`${slotBase({ className })}`} data-comp={compName} use:useWaypoint data-waypoint>
+  <div class={slotInner()} data-waypoint-target>
+    <div class={slotBigText()} use:useFullWidthText><span class="leading-[1]!">LÄUFT</span></div>
+  </div>
 </footer>
