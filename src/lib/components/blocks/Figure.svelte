119f1c68a9733ec49eba670bc30f583463b2caeb
David Hellmann <david@hellmann.io>
2025-04-27 21:09:49 +0200

    chore: fix code padding

--- src/lib/components/blocks/Figure.svelte
+++ src/lib/components/blocks/Figure.svelte
@@ -2,13 +2,13 @@
   import { tv, type VariantProps } from "tailwind-variants";
   import Caption from "$components/text/Caption.svelte";
   import RichText from "$components/text/RichText.svelte";
-  import type{  Snippet } from "svelte";
+  import type { Snippet } from "svelte";
 
   const tvFigure = tv({
     slots: {
-      slotFigure: "flex flex-col gap-fluid",
+      slotFigure: "flex flex-col gap-8 md:gap-fluid",
       slotFigcaption: "",
-      slotRichText: "text-sm",
+      slotRichText: "text-sm"
     },
     variants: {
       variant: {
@@ -17,8 +17,8 @@
         },
         boxed: {
           slotFigure: "bg-neutral-300/60 rounded-3xl",
-          slotFigcaption: "pb-fluid px-fluid",
-          slotRichText: "pb-fluid px-fluid",
+          slotFigcaption: "pb-8 md:pb-fluid px-8 md:px-fluid",
+          slotRichText: "pb-8 md:pb-fluid px-8 md:px-fluid"
         }
       }
     },
@@ -53,20 +53,11 @@
   const { slotFigure, slotFigcaption, slotRichText } = tvFigure({ variant, className });
 </script>
 
-<figure
-  data-comp={compName}
-  class={slotFigure({ variant, className })}
->
+<figure data-comp={compName} class={slotFigure({ variant, className })}>
   {@render children?.()}
   {#if text}
-    <Caption
-      text={text}
-      headline={headline}
-      source={source}
-      sourceUrl={sourceUrl}
-      className={slotFigcaption()}
-    />
+    <Caption {text} {headline} {source} {sourceUrl} className={slotFigcaption()} />
   {:else if html}
-    <RichText html={html} className={slotRichText()} />
+    <RichText {html} className={slotRichText()} />
   {/if}
 </figure>
