ae364779b30f95de8ac0c6a84aa66f9f56c9013b
David Hellmann <david@hellmann.io>
2025-06-14 17:13:55 +0200

    chore

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,20 +1,23 @@
 module.exports = {
-  apps: [{
-    name: 'sveltekit-app',
-    script: 'build/index.js',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'production',
-      PORT: 3000,
-      HOST: '0.0.0.0'
-    },
-    env_production: {
-      NODE_ENV: 'production',
-      PORT: 3000,
-      HOST: '0.0.0.0'
+  apps: [
+    {
+      name: "sveltekit-app",
+      script: "build/index.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+        HOST: "0.0.0.0"
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 3000,
+        HOST: "0.0.0.0",
+        ORIGIN: "https://sveltekit.davidhellmann.com"
+      }
     }
-  }]
+  ]
 };
