d86a51fe91f15edd5467f9cfa3f1be4c62377059
David Hellmann <david@hellmann.io>
2025-06-19 22:54:38 +0200

    chore: pnpm update / add types

---
+++ src/lib/types/lightgallery.d.ts
@@ -0,0 +1,11 @@
+declare module "lightgallery/plugins/thumbnail/lg-thumbnail.min" {
+  import { LightGalleryPlugin } from "lightgallery/types";
+  const lgThumbnail: LightGalleryPlugin;
+  export default lgThumbnail;
+}
+
+declare module "lightgallery/plugins/zoom/lg-zoom.min" {
+  import { LightGalleryPlugin } from "lightgallery/types";
+  const lgZoom: LightGalleryPlugin;
+  export default lgZoom;
+}
