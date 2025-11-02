19d051b08e6919c7ccfef8316dfb802213611ada
David Hellmann <david@hellmann.io>
2025-11-02 16:09:40 +0100

    style: code

--- src/lib/components/media/Image.svelte
+++ src/lib/components/media/Image.svelte
@@ -6,7 +6,7 @@
   import { useUnlazy } from "$lib/actions/action.unlazy";
   import { tv, type VariantProps } from "tailwind-variants";
   import type { Asset_CustomFieldsFragment, Asset_DataFragment, Asset_TransformsFragment } from "$graphql/graphql";
-  import { ComponentProps } from "svelte";
+  import type { ComponentProps } from "svelte";
 
   type ObjectFit = "cover" | "contain" | "fill" | "none" | "scale-down";
 
