2cd635745faf8eb111a257cba09a752497566090
David Hellmann <david@hellmann.io>
2026-08-05 12:04:29 +0200

    chore

--- svelte.config.js
+++ svelte.config.js
@@ -14,6 +14,7 @@ const config = {
       out: "build",
       precompress: false
     }),
+    inlineStyleThreshold: 5000,
     alias: {
       $components: "src/lib/components",
       $graphql: "src/lib/graphql",
